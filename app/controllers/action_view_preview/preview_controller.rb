2e9187a8dc3a0cfe37fd55317bf0c9633d616a99
Lucas Campanari <campanari.ls@gmail.com>
2021-04-24 19:33:23 -0400

    chore: initial controller and views

--- app/controllers/action_view_preview/preview_controller.rb
+++ app/controllers/action_view_preview/preview_controller.rb
@@ -3,6 +3,8 @@ require_dependency "action_view_preview/application_controller"
 module ActionViewPreview
   class PreviewController < ApplicationController
     def index
+      @previews = ActionViewPreview::Preview.all
+      @page_title = "View Previews"
     end
 
     def show
--- lib/action_view_preview.rb
+++ lib/action_view_preview.rb
@@ -2,5 +2,41 @@ require "action_view_preview/version"
 require "action_view_preview/engine"
 
 module ActionViewPreview
-  # Your code goes here...
+  class Preview
+    extend ActiveSupport::DescendantsTracker
+
+    attr_reader :params
+
+    class << self
+      # Returns all preview classes
+      def all
+        load_previews if descendants.empty?
+        descendants
+      end
+
+      # Returns all of the available previews
+      def previews
+        public_instance_methods(false).map(&:to_s).sort
+      end
+
+      # Returns the underscored name of the mailer preview without the suffix
+      def preview_name
+        name.delete_suffix("Preview").underscore
+      end
+
+      private
+
+      def load_previews
+        if preview_path
+          Dir["#{preview_path}/**/*_preview.rb"].sort.each { |file| require_dependency file }
+        end
+      end
+
+      def preview_path
+        # Base.preview_path
+        "#{Rails.root}/test/views/previews"
+      end
+    end
+  end
+
 end
