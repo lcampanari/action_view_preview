01f5cd28274658e0fefff2a53c8f71289a9d5edc
Lucas Campanari <campanari.ls@gmail.com>
2021-08-29 17:42:42 -0300

    chore: make routes internal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ActionViewPreview::Engine.routes.draw do
-  get '/'      => 'preview#index', as: :preview_index
-  get '/*path' => 'preview#show',  as: :preview_show
+  get '/'      => 'preview#index', as: :preview_index, internal: true
+  get '/*path' => 'preview#show',  as: :preview_show, internal: true
 end
