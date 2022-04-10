dec85fd9bfe92290b0c76b26d794d9e97985bc33
Lucas Campanari <campanari.ls@gmail.com>
2022-04-09 22:38:53 -0400

    fix: add preview path to autoloads

--- lib/action_view_preview.rb
+++ lib/action_view_preview.rb
@@ -1,19 +1,16 @@
 require "action_view_preview/version"
-require "action_view_preview/engine"
+require "active_support/dependencies/autoload"
 
 module ActionViewPreview
   extend ::ActiveSupport::Autoload
   
   autoload :Base
 
-  mattr_accessor :preview_path
+  mattr_accessor :preview_path, instance_writer: false
 
   def self.setup
     yield self
   end
-
-  def self.preview_path
-    @@preview_path || "#{Rails.root}/test/views/previews"
-  end
- 
 end
+
+require "action_view_preview/engine" if defined?(Rails::Engine)
--- lib/action_view_preview/engine.rb
+++ lib/action_view_preview/engine.rb
@@ -1,5 +1,32 @@
+# frozen_string_literal: true
+
+require "rails"
+
 module ActionViewPreview
   class Engine < ::Rails::Engine
     isolate_namespace ActionViewPreview
+
+    config.action_view_preview = ActionViewPreview
+    config.eager_load_namespaces << ActionViewPreview
+
+    initializer "action_view_preview.set_configs" do |app|
+      options = app.config.action_view_preview
+
+      options.preview_path ||= defined?(Rails.root) ? "#{Rails.root}/test/views/previews" : nil
+      
+      ActiveSupport.on_load(:action_view_preview) do
+        options.each { |k, v| ActionViewPreview.send("#{k}=", v) }
+      end
+    end
+
+    initializer "action_view_preview.set_autoload_paths" do |app|
+      options = app.config.action_view_preview
+
+      if options.preview_path
+        ActiveSupport::Dependencies.autoload_paths << options.preview_path
+      end
+    end
   end
 end
+
+require 'action_view_preview' if !defined?(ActionViewPreview::Base)
